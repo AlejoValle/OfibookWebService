f32d1bb0690adeeb1984f72ae6d14726b31ad48c
José Alejandro Ramírez Valle <00139121@uca.edu.sv>
2022-11-27 21:39:18 -0600

    sdadtyuvasd

--- ofibook-api/app.js
+++ ofibook-api/app.js
@@ -3,11 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require("cors");
+const netlify = require("./netlify/functions/conectionxd");
 
 const apiRouter = require("./routes/api/index.router");
 
 var app = express();
 
+netlify.connect();
 
 app.use(cors())
 app.use(logger("dev"));
