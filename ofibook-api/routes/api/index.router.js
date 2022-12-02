2a307bc6f3f0bd7bbd4470cda52370b343440108
AlejoValle <00139121@uca.edu.sv>
2022-12-02 01:28:34 -0600

    Laboratorio-3

--- ofibook-api/routes/api/index.router.js
+++ ofibook-api/routes/api/index.router.js
@@ -4,9 +4,11 @@ const router = express.Router();
 //importar todos los enrutadores
 const postRouter = require("./post.router");
 const authRouter = require("./auth.router");
+const prfRouter = require("./prof.router");
 
 //Definir las rutas
 router.use("/auth", authRouter);
 router.use("/post", postRouter);
+router.use("/prof", prfRouter);
 
 module.exports = router;
